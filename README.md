6d3adcc8a9688087cce55954318dc52ad088507d
Emin Muhammadi <muemin17631@sabah.edu.az>
2022-02-06 21:12:33 +0100

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # go-tcp
 Transmission Control Protocol in Golang
+
+## Installation
+```bash
+go build .
+```
+
+## Usage
+
+### Server
+```bash
+./go-tcp --ip 127.0.0.1 --port 8080 --mode server
+```
+
+### Client
+```bash
+./go-tcp --ip 127.0.0.1 --port 8080 --mode client --data "Hello World!"
+```
--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 var ip = flag.String("ip", "0.0.0.0", "--ip 0.0.0.0")
 var port = flag.String("port", "8080", "--port 8080")
 var data = flag.String("data", "", "--data \"Hello World\"")
-var status = flag.String("status", "server", "--status server/client")
+var mode = flag.String("mode", "server", "--mode server/client")
 
 func main() {
 	flag.Parse()
 
-	if *status == "server" {
+	if *mode == "server" {
 		if err := network.Server(*ip, *port); err != nil {
 			panic(err)
 		}
 	}
 
-	if *status == "client" {
+	if *mode == "client" {
 		if err := network.Client(*ip, *port, *data); err != nil {
 			panic(err)
 		}
